d48d7734f441ca1ab388c3d6da6132980b2ec813
kamranrk <kamran16alam@gmail.com>
2020-09-14 20:02:03 +0530

    added a form for adding our notes and background

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-;
+import React, { Component } from 'react';
 
-function App() {
-  return <div className = "App">WELCOME</div>
+class App extends Component{
+  render(){
+    return (
+      <div className="container-fluid">
+           <div className="row">
+             <div className="col-sm-6 col-sm-offset-3">
+               <form>
+                 <div className="form-group">
+                   <input type="text" 
+                   name="title" 
+                   className="form-control no-border"
+                   placeholder="Your title here"
+                   required />  
+                 </div>
+                 <div className="form-group">
+                   <textarea type="text" 
+                   name="body" 
+                   className="form-control no-border"
+                   placeholder="Your body here"
+                   required />  
+                 </div>
+                 <div className="form-group">
+                   <button className="btn btn-primary col-cm-12">SAVE AS NOTE</button>
+                 </div>
+               </form>
+             </div>
+           </div>
+      </div>
+    )
+  }
 }
 
 export default App;
