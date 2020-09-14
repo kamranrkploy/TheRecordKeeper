1227f8e405cdfbde686350e2bdebfe079ca7c0d2
kamranrk <kamran16alam@gmail.com>
2020-09-14 20:57:17 +0530

    styling form

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,32 @@ import React, { Component } from 'react';
 class App extends Component{
   render(){
     return (
-      <div className="container-fluid">
-           <div className="row">
-             <div className="col-sm-6 col-sm-offset-3">
+      <div className="grid">
+           <div className="IP">
+             <div className="I1">
                <form>
-                 <div className="form-group">
+                 <div className="I2">
                    <input type="text" 
                    name="title" 
                    className="form-control no-border"
                    placeholder="Your title here"
                    required />  
                  </div>
-                 <div className="form-group">
+                 <div className="I3">
                    <textarea type="text" 
                    name="body" 
                    className="form-control no-border"
                    placeholder="Your body here"
                    required />  
                  </div>
-                 <div className="form-group">
-                   <button className="btn btn-primary col-cm-12">SAVE AS NOTE</button>
+                 <div className="I4">
+                   <button className="">SAVE AS NOTE</button>
                  </div>
                </form>
              </div>
            </div>
       </div>
-    )
+    );
   }
 }
 
