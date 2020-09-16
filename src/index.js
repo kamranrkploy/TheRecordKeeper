a9b844181916820dee42c2c414314fa97a2c7151
kamranrk <kamran16alam@gmail.com>
2020-09-16 18:13:24 +0530

    added required packe and created redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './components/App';
 
+import {createStore , applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+//creating store
+const store = createStore(composeWithDevTools());
+
+//providing store to react using Provider
+
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
