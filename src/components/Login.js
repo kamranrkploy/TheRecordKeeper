14b853c504a35984920c0729a1f91b785e41e13f
kamranrk <kamran16alam@gmail.com>
2020-09-18 20:20:36 +0530

    created login.js and added methods for login

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+
+class Login extends Component {
+    render(){
+        return  (
+            <div className="Lcontainer">
+                    <div>
+                        <div><h1>Slect a Login Option</h1></div>
+                        <div><button>Google</button></div>
+                        <div><button>Twitter</button></div>
+                    </div>
+            </div>
+        )
+    }
+}
